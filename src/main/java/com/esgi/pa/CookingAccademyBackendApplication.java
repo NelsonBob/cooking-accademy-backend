2ca664fdfb9cb183be2e5d04086c2501d665405a
NelsonBob <nfomekongnoagni@myges.fr>
2023-08-21 15:39:16 +0200

    init

---
+++ src/main/java/com/esgi/pa/CookingAccademyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingAccademyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingAccademyBackendApplication.class, args);
+	}
+
+}
